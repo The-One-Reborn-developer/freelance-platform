fbe71020519df6cad4e084bbb17572b61b865731
The One Reborn <mortonmarrion@gmail.com>
2024-11-16 17:12:37 +0300

    fix: video_file_id tag in chat txt file

--- app/scripts/save_customer_chat_message.py
+++ app/scripts/save_customer_chat_message.py
@@ -15,7 +15,7 @@ def save_customer_chat_message(bid_id: int,
     file_path = os.path.join(folder_path, file_name)
 
     separator = "\n---\n"
-    formatted_message = f"Сообщение от заказчика {customer_full_name} подрядчику {performer_full_name}:\n\n{message}\n{video_file_id}{separator}"
+    formatted_message = f"Сообщение от заказчика {customer_full_name} подрядчику {performer_full_name}:\n\n{message}\nvideo_file_id:{video_file_id}{separator}"
 
     with open(file_path, "a", encoding="utf-8") as file:
         file.write(formatted_message)
