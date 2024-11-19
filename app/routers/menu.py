23f7e1484c0a5ed9c0b2dfd4f7cb28e767793bb0
The One Reborn <mortonmarrion@gmail.com>
2024-11-19 18:15:13 +0300

    fix: separate keyboard for customer/performer

--- app/routers/menu.py
+++ app/routers/menu.py
@@ -19,12 +19,14 @@ async def menu_callback_handler(callback: CallbackQuery):
     user = get_user_by_telegram_id_task.delay(callback.from_user.id).get()
 
     if user != [] and user is not None:
+        keyboard = None
+
         if user[4]:
             keyboard = customer_menu_keyboard()
         elif user[3]:
             keyboard = performer_menu_keyboard()
 
-        await callback.message.answer(choose_option(), reply_markup=both_menu_keyboard()) # TODO: REPLACE BACK TO SEPARATE KEYBOARDS AFTER DEVELOPMENT
+        await callback.message.answer(choose_option(), reply_markup=keyboard) # TODO: REPLACE BACK TO SEPARATE KEYBOARDS AFTER DEVELOPMENT
         #await callback.message.answer(content, reply_markup=both_menu_keyboard())
     else:
         await callback.answer(general(), show_alert=True)
